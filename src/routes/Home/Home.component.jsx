df6397159ebb6837067cf358153f589f7a8f2c8e
Ye Myint Kyaw <yemyintkyaw241220@gmail.com>
2023-12-03 11:19:46 +0630

    update

--- src/routes/Home/Home.component.jsx
+++ src/routes/Home/Home.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CategoryContainer from "../../Components/Category-container/Category-container.component.jsx";
+import CategoryContainer from "../../Components/Category-container/Category-container.component";
 
 const Home = () => {
   return <div>{<CategoryContainer />}</div>;
