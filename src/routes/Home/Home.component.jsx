0adad7bf07ce054355ce336cbacf90b83306cdad
Ye Myint Kyaw <yemyintkyaw241220@gmail.com>
2023-12-03 10:54:05 +0630

    update the path

--- src/routes/Home/Home.component.jsx
+++ src/routes/Home/Home.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CategoryContainer from "../../Components/Category-container/Category-container.component";
+import CategoryContainer from "../../Components/Category-container/Category-container.component.jsx";
 
 const Home = () => {
   return <div>{<CategoryContainer />}</div>;
